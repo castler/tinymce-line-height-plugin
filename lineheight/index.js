a1ccd431d56f736d550511aa96bae55de2299c20
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2019-06-08 23:12:36 +0800

    Upgrade to TinyMCE 5

---
+++ lineheight/index.js
@@ -0,0 +1,7 @@
+// Exports the "lineheight" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/lineheight')
+//   ES2015:
+//     import 'tinymce/plugins/lineheight'
+require('./plugin.js');
