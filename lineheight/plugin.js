3363301e546423a39fa989ec1e78e35541011535
jenia <jenian@welldone-software.com>
2019-10-27 14:10:59 +0200

    fix icon

--- lineheight/plugin.js
+++ lineheight/plugin.js
@@ -11,16 +11,16 @@
           styles: { 'line-height': '%value' }
         }
       });
+    });
 
-      editor.ui.registry.addIcon(
-        'line-height',
-        `
+    editor.ui.registry.addIcon(
+      'line-height',
+      `
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M9.984 12.984v-1.969h12v1.969h-12zM9.984 18.984v-1.969h12v1.969h-12zM9.984 5.016h12v1.969h-12v-1.969zM6 6.984v10.031h2.484l-3.469 3.469-3.516-3.469h2.484v-10.031h-2.484l3.516-3.469 3.469 3.469h-2.484z"></path>
           </svg>
         `
-      );
-    });
+    );
 
     editor.ui.registry.addMenuButton('lineheightselect', {
       tooltip: 'Line height',
